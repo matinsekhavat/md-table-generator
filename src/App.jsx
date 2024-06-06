397bd102804f9bed3afbfc8868997124b569d607
matinsekhavat <matinhyphen@gmail.com>
2024-06-06 19:01:48 +0330

    implement Tablesize logic & sync to TableSize component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import TableSizeCompute from "./components/TableSizeCompute";
 
 function App() {
+  const [tableSize, setTableSize] = useState({
+    rows: 1,
+    columns: 1,
+  });
+  function tableSizeHandler(e) {
+    const { name, value } = e.target;
+    setTableSize((size) => ({ ...size, [name]: value }));
+  }
   return (
     <>
       <Header />
+      <main>
+        <TableSizeCompute placeholder="rows" onTableSize={tableSizeHandler} />
+        <TableSizeCompute
+          placeholder="columns"
+          onTableSize={tableSizeHandler}
+        />
+      </main>
     </>
   );
 }
