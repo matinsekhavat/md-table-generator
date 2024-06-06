e39fcd78fe0c0570b2c62aa5c6e194b28865d357
matinsekhavat <matinhyphen@gmail.com>
2024-06-06 18:24:40 +0330

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="py-4 bg-stone-100">
+      <h1 className="text-center text-2xl font-bold">
+        Markdown Table Generator
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
